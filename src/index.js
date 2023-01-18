96629dc930c28836449967aeb641939e7093b660
glorymugizi <glorymugizi@gmail.com>
2023-01-18 11:32:58 +0300

    Edit the diplaytask.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import _ from "lodash";
-import "./style.css";
+// import _ from "lodash";
+import './style.css';
+import todoArr from './modules/tasks.js';
+
+const toDoItems = document.querySelector('.to-do-items');
+
+todoArr.forEach((task) => {
+  const { description } = task;
+  toDoItems.innerHTML += `<div>
+    <input type="checkbox" id="" name="" />
+    <label for="" name="">${description}</label>
+  </div>`;
+});
---
+++ src/modules/tasks.js
@@ -0,0 +1,18 @@
+const todoArr = [
+  {
+    description: 'Morning session meeting',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Coding practice',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Exercise',
+    completed: true,
+    index: 2,
+  },
+];
+export default todoArr;
